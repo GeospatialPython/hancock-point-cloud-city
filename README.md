52253bd29701ae0065bcda861acd27388cee5d86
Joel Lawhead <jlawhead@nvs-inc.com>
2019-01-05 22:51:38 -0600

    NVS Building v1

--- README.md
+++
@@ -1,2 +0,0 @@
-# hancock-point-cloud-city
- 2018 NIST Public Safety Innovation Accelerator Program Point Cloud City Project for Hancock County
---
+++ cloud.js
@@ -0,0 +1,29 @@
+{
+    "version": "1.7",
+    "octreeDir": "data",
+    "projection": "",
+    "points": 15797102,
+    "boundingBox": {
+        "lx": -37.3745002746582,
+        "ly": -31.312400817871095,
+        "lz": -1.0819000005722047,
+        "ux": 47.71630096435547,
+        "uy": 53.77840042114258,
+        "uz": 84.00890123844147
+    },
+    "tightBoundingBox": {
+        "lx": -37.3745,
+        "ly": -31.3124,
+        "lz": -1.0819,
+        "ux": 35.12230000000001,
+        "uy": 53.778400000000008,
+        "uz": 1.4180000000000002
+    },
+    "pointAttributes": [
+        "POSITION_CARTESIAN",
+        "COLOR_PACKED"
+    ],
+    "spacing": 0.736907958984375,
+    "scale": 0.001,
+    "hierarchyStepSize": 5
+}
